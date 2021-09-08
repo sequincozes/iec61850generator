f5130fbb40d508d01426565c889b8bfabc707b25
sequincozes <sequincozes@gmail.com>
2021-09-08 17:42:34 -0300

    v7 done - final version 03/08/2021

--- src/main/java/uff/midiacom/usecases/AbstractUseCase.java
+++ src/main/java/uff/midiacom/usecases/AbstractUseCase.java
@@ -199,63 +199,65 @@ public abstract class AbstractUseCase extends SVFeatureComposer {
         write("@attribute vsmB numeric"); //SV-related 12
         write("@attribute vsmC numeric"); //SV-related 13
 
-        write("@attribute isbARmsValue numeric"); //SV-related 2
-        write("@attribute isbBRmsValue numeric"); //SV-related 2
-        write("@attribute isbCRmsValue numeric"); //SV-related 2
-        write("@attribute ismARmsValue numeric"); //SV-related 2
-        write("@attribute ismBRmsValue numeric"); //SV-related 2
-        write("@attribute ismCRmsValue numeric"); //SV-related 2
-        write("@attribute vsbARmsValue numeric"); //SV-related 2
-        write("@attribute vsbBRmsValue numeric"); //SV-related 2
-        write("@attribute vsbCRmsValue numeric"); //SV-related 2
-        write("@attribute vsmARmsValue numeric"); //SV-related 2
-        write("@attribute vsmBRmsValue numeric"); //SV-related 2
-        write("@attribute vsmCRmsValue numeric"); //SV-related 2
-        write("@attribute isbATrapAreaSum numeric"); //SV-related 2
-        write("@attribute isbBTrapAreaSum numeric"); //SV-related 2
-        write("@attribute isbCTrapAreaSum numeric"); //SV-related 2
-        write("@attribute ismATrapAreaSum numeric"); //SV-related 2
-        write("@attribute ismBTrapAreaSum numeric"); //SV-related 2
-        write("@attribute ismCTrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsbATrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsbBTrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsbCTrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsmATrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsmBTrapAreaSum numeric"); //SV-related 2
-        write("@attribute vsmCTrapAreaSum numeric"); //SV-related 2
+        write("@attribute isbARmsValue numeric"); //SV-related 14
+        write("@attribute isbBRmsValue numeric"); //SV-related 15
+        write("@attribute isbCRmsValue numeric"); //SV-related 16
+        write("@attribute ismARmsValue numeric"); //SV-related 17
+        write("@attribute ismBRmsValue numeric"); //SV-related 18/
+        write("@attribute ismCRmsValue numeric"); //SV-related 19
+        write("@attribute vsbARmsValue numeric"); //SV-related 20
+        write("@attribute vsbBRmsValue numeric"); //SV-related 21
+        write("@attribute vsbCRmsValue numeric"); //SV-related 22
+        write("@attribute vsmARmsValue numeric"); //SV-related 23
+        write("@attribute vsmBRmsValue numeric"); //SV-related 24
+        write("@attribute vsmCRmsValue numeric"); //SV-related 25
 
-        write("@attribute t numeric"); // time-based  14
-        write("@attribute GooseTimestamp numeric"); // time-based 15
-        write("@attribute SqNum numeric"); // Status-based 16
-        write("@attribute StNum numeric"); // Status-based 17
-        write("@attribute cbStatus numeric"); // Status-based 18
-        write("@attribute frameLen numeric"); //network-based 19
-        write("@attribute ethDst {01:a0:f4:08:2f:77, FF:FF:FF:FF:FF:11, FF:FF:FF:FF:FF:22, FF:FF:FF:FF:FF:33, FF:FF:FF:FF:FF:44, FF:FF:FF:FF:FF:55, FF:FF:FF:FF:FF:66, FF:FF:FF:FF:FF:FF, FF:FF:FF:FF:FF:77, FF:FF:FF:FF:FF:AA, FF:FF:FF:FF:FF:BB, FF:FF:FF:FF:FF:CC, FF:FF:FF:FF:FF:DD, FF:FF:FF:FF:FF:EE, FF:FF:FF:FF:FF:AB, FF:FF:FF:FF:FF:AC}"); //network-based 20
-        write("@attribute ethSrc {00:a0:f4:08:2f:77, FF:FF:FF:FF:FF:11, FF:FF:FF:FF:FF:22, FF:FF:FF:FF:FF:33, FF:FF:FF:FF:FF:44, FF:FF:FF:FF:FF:55, FF:FF:FF:FF:FF:66, FF:FF:FF:FF:FF:FF, FF:FF:FF:FF:FF:77, FF:FF:FF:FF:FF:AA, FF:FF:FF:FF:FF:BB, FF:FF:FF:FF:FF:CC, FF:FF:FF:FF:FF:DD, FF:FF:FF:FF:FF:EE, FF:FF:FF:FF:FF:AB, FF:FF:FF:FF:FF:AC}"); //network-based 21
-        write("@attribute ethType {0x000077b7, 0x000088b8}"); //network-based 22
-        write("@attribute gooseTimeAllowedtoLive numeric"); //IED-based 23
-        write("@attribute gooseAppid {0x00003002, 0x00003001}");  //IED-based 24
-        write("@attribute gooseLen numeric");  //IED-based 25
-        write("@attribute TPID {0x7101, 0x8100}");  //IED-based 26
-        write("@attribute gocbRef {LD/LLN0$IntLockB, LD/LLN0$GO$gcbA}");  //IED-based 27
-        write("@attribute datSet {LD/LLN0$IntLockA, AA1C1Q01A1LD0/LLN0$InterlockingC}");  //IED-based 28
-        write("@attribute goID {InterlockingF, InterlockingA}");  //IED-based 29
-        write("@attribute test {TRUE, FALSE}");  //IED-based 30
-        write("@attribute confRev numeric");  //IED-based 31
-        write("@attribute ndsCom {TRUE, FALSE}");  //IED-based 32
-        write("@attribute numDatSetEntries numeric");  //IED-based 33
-        write("@attribute APDUSize numeric"); //network-based 34
-        write("@attribute protocol {SV, GOOSE}"); //network-based 35
-        write("@attribute stDiff numeric"); // temporal consistency 36
-        write("@attribute sqDiff numeric"); // temporal consistency 37
-        write("@attribute gooseLengthDiff numeric"); // temporal consistency 38
-        write("@attribute cbStatusDiff numeric"); // temporal consistency 39
-        write("@attribute apduSizeDiff numeric"); // temporal consistency 40
-        write("@attribute frameLengthDiff numeric"); // temporal consistency 41
-        write("@attribute timestampDiff numeric"); // temporal consistency 42
-        write("@attribute tDiff numeric"); // temporal consistency 43
-        write("@attribute timeFromLastChange numeric"); // temporal consistency 44
-        write("@attribute delay numeric"); // temporal consistency 45
+        write("@attribute isbATrapAreaSum numeric"); //SV-related 26
+        write("@attribute isbBTrapAreaSum numeric"); //SV-related 27
+        write("@attribute isbCTrapAreaSum numeric"); //SV-related 28
+        write("@attribute ismATrapAreaSum numeric"); //SV-related 29
+        write("@attribute ismBTrapAreaSum numeric"); //SV-related 30
+        write("@attribute ismCTrapAreaSum numeric"); //SV-related 31
+        write("@attribute vsbATrapAreaSum numeric"); //SV-related 32
+        write("@attribute vsbBTrapAreaSum numeric"); //SV-related 33
+        write("@attribute vsbCTrapAreaSum numeric"); //SV-related 34
+        write("@attribute vsmATrapAreaSum numeric"); //SV-related 35
+        write("@attribute vsmBTrapAreaSum numeric"); //SV-related 36
+        write("@attribute vsmCTrapAreaSum numeric"); //SV-related 37
+
+        write("@attribute t numeric"); // time-based  38
+        write("@attribute GooseTimestamp numeric"); // time-based 39
+        write("@attribute SqNum numeric"); // Status-based 40
+        write("@attribute StNum numeric"); // Status-based 41
+        write("@attribute cbStatus numeric"); // Status-based 42
+        write("@attribute frameLen numeric"); //network-based 43
+        write("@attribute ethDst {01:a0:f4:08:2f:77, FF:FF:FF:FF:FF:11, FF:FF:FF:FF:FF:22, FF:FF:FF:FF:FF:33, FF:FF:FF:FF:FF:44, FF:FF:FF:FF:FF:55, FF:FF:FF:FF:FF:66, FF:FF:FF:FF:FF:FF, FF:FF:FF:FF:FF:77, FF:FF:FF:FF:FF:AA, FF:FF:FF:FF:FF:BB, FF:FF:FF:FF:FF:CC, FF:FF:FF:FF:FF:DD, FF:FF:FF:FF:FF:EE, FF:FF:FF:FF:FF:AB, FF:FF:FF:FF:FF:AC}"); //network-based 44
+        write("@attribute ethSrc {00:a0:f4:08:2f:77, FF:FF:FF:FF:FF:11, FF:FF:FF:FF:FF:22, FF:FF:FF:FF:FF:33, FF:FF:FF:FF:FF:44, FF:FF:FF:FF:FF:55, FF:FF:FF:FF:FF:66, FF:FF:FF:FF:FF:FF, FF:FF:FF:FF:FF:77, FF:FF:FF:FF:FF:AA, FF:FF:FF:FF:FF:BB, FF:FF:FF:FF:FF:CC, FF:FF:FF:FF:FF:DD, FF:FF:FF:FF:FF:EE, FF:FF:FF:FF:FF:AB, FF:FF:FF:FF:FF:AC}"); //network-based 45
+        write("@attribute ethType {0x000077b7, 0x000088b8}"); //network-based 46
+        write("@attribute gooseTimeAllowedtoLive numeric"); //IED-based 47
+        write("@attribute gooseAppid {0x00003002, 0x00003001}");  //IED-based 48
+        write("@attribute gooseLen numeric");  //IED-based 49
+        write("@attribute TPID {0x7101, 0x8100}");  //IED-based 50
+        write("@attribute gocbRef {LD/LLN0$IntLockB, LD/LLN0$GO$gcbA}");  //IED-based 51
+        write("@attribute datSet {LD/LLN0$IntLockA, AA1C1Q01A1LD0/LLN0$InterlockingC}");  //IED-based 52
+        write("@attribute goID {InterlockingF, InterlockingA}");  //IED-based 53
+        write("@attribute test {TRUE, FALSE}");  //IED-based 54
+        write("@attribute confRev numeric");  //IED-based 55
+        write("@attribute ndsCom {TRUE, FALSE}");  //IED-based 56
+        write("@attribute numDatSetEntries numeric");  //IED-based 57
+        write("@attribute APDUSize numeric"); //network-based 58
+
+        write("@attribute protocol {SV, GOOSE}"); //network-based 59
+        write("@attribute stDiff numeric"); // temporal consistency 60
+        write("@attribute sqDiff numeric"); // temporal consistency 61
+        write("@attribute gooseLengthDiff numeric"); // temporal consistency 62
+        write("@attribute cbStatusDiff numeric"); // temporal consistency 63
+        write("@attribute apduSizeDiff numeric"); // temporal consistency 64
+        write("@attribute frameLengthDiff numeric"); // temporal consistency 65
+        write("@attribute timestampDiff numeric"); // temporal consistency 66
+        write("@attribute tDiff numeric"); // temporal consistency 67
+        write("@attribute timeFromLastChange numeric"); // temporal consistency 68
+        write("@attribute delay numeric"); // temporal consistency 69
         String classLine = "@attribute @class@ {"
                 + label[0] + ", "
                 + label[1] + ", "
